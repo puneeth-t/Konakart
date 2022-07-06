243f134640bb3992905b2be8dc241d3e475eac84
bharatNakka <nbarath2009@gmail.com>
2022-07-06 13:11:22 +0000

    Update CustomTilesListener.java
    VERITA-126

--- src/main/java/com/CustomTilesListener.java
+++ src/main/java/com/CustomTilesListener.java
@@ -3,7 +3,9 @@ package com;
 import org.apache.tiles.startup.TilesInitializer;
 import org.apache.tiles.web.startup.AbstractTilesListener;
 
-public class CustomTilesListener extends AbstractTilesListener {
+//Customtiles
+public class CustomTilesListener
+	extends AbstractTilesListener {
 
 	@Override
 	protected TilesInitializer createTilesInitializer() {
